cdb18fae290843286be3251b7c018e508688737c
Jim Martino <james.r.martino@gmail.com>
2018-01-17 13:34:57 -0500

    initial model commit: Grant, JHU_Grant, Identifier, Person

---
+++ pass-grant-model/src/main/java/Grant.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2018 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.joda.time.DateTime;
+
+import java.util.List;
+
+public class Grant {
+
+    private Identifier id;
+    private String awardNumber;
+    private String projectName;
+    private String funder;
+    private DateTime startDate;
+    private DateTime endDate;
+    private List<Identifier> alternateIds;
+    private Person creator;
+    private DateTime creationDate;
+    private Person pi;
+    private List<Person> copis;
+
+    public enum  status {
+        ACTIVE,
+        PRE_AWARD,
+        TERMINATED
+    }
+
+    public enum compliance {
+        YES,
+        NO,
+        IN_PROGRESS
+    }
+
+    public Identifier getId() {
+        return id;
+    }
+
+    public void setId(Identifier id) {
+        this.id = id;
+    }
+
+    public String getAwardNumber() {
+        return awardNumber;
+    }
+
+    public void setAwardNumber(String awardNumber) {
+        this.awardNumber = awardNumber;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getFunder() {
+        return funder;
+    }
+
+    public void setFunder(String funder) {
+        this.funder = funder;
+    }
+
+    public DateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(DateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public DateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(DateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Identifier> getAlternateIds() {
+        return alternateIds;
+    }
+
+    public void setAlternateIds(List<Identifier> alternateIds) {
+        this.alternateIds = alternateIds;
+    }
+
+    public Person getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Person creator) {
+        this.creator = creator;
+    }
+
+    public DateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(DateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Person getPi() {
+        return pi;
+    }
+
+    public void setPi(Person pi) {
+        this.pi = pi;
+    }
+
+    public List<Person> getCopis() {
+        return copis;
+    }
+
+    public void setCopis(List<Person> copis) {
+        this.copis = copis;
+    }
+
+
+}
---
+++ pass-grant-model/src/main/java/JHU_Grant.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class JHU_Grant extends Grant {
+    private String school; //could be enum?
+    private String department;
+    private String organizationalUnit;
+
+    public JHU_Grant(String school, String department, String organizationalUnit) {
+        this.school = school;
+        this.department = department;
+        this.organizationalUnit = organizationalUnit;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getOrganizationalUnit() {
+        return organizationalUnit;
+    }
+
+    public void setOrganizationalUnit(String organizationalUnit) {
+        this.organizationalUnit = organizationalUnit;
+    }
+}
