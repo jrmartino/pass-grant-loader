29c10e55f39eb1f46a9f441841de5fac6050cbc2
Jim Martino <james.r.martino@gmail.com>
2018-10-01 10:17:22 -0400

    cleanup - remove unused imports

--- pass-grant-data/src/main/java/org/dataconservancy/pass/grant/data/PassEntityUtil.java
+++ pass-grant-data/src/main/java/org/dataconservancy/pass/grant/data/PassEntityUtil.java
@@ -20,10 +20,8 @@ import org.dataconservancy.pass.model.Funder;
 import org.dataconservancy.pass.model.Grant;
 import org.dataconservancy.pass.model.User;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
